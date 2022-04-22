fc7ef16028834c7f03eadd5594d3eb59d7ea491b
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-04-22 09:20:17 +0700

    .....

--- bai_1/js/index.js
+++ bai_1/js/index.js
@@ -1,8 +1,13 @@
-let arr = [6,7,8,6,8,9,7,9,8]
-
-function findScore(arr) {
-    let max = 
+function thirdMax(arr){
     for (let i = 0; i < arr.length; i++) {
-
+        for (let j = i+1; j < arr.length; j++) {
+            if(arr[i]>arr[j]){
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp
+            }
+        }
     }
+    return arr[arr.length-3];
 }
+
